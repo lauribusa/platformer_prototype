c0a08a873e4365b16ebea238ffb14cc963963022
9640_gdev (Student) <s_9640_gdev@pedagogique.lan>
2019-11-04 17:26:17 +0100

    init commit

---
+++ Scripts/MovementController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementController : MonoBehaviour
+{
+	BoxCollider2D boxCollider;
+	Vector2 bottomLeft, bottomRight, topLeft, topRight;
+
+	public int horizontalRayCount;
+	public int verticalRayCount;
+	public LayerMask _layerMask;
+
+	float verticalRaySpacing;
+    // Start is called before the first frame update
+    void Start()
+    {
+		boxCollider = GetComponent<BoxCollider2D>();
+		verticalRaySpacing = boxCollider.bounds.size.y / (verticalRayCount - 1);
+    }
+
+	void ComputeBounds()
+	{
+		bottomLeft = new Vector2(boxCollider.bounds.min.x, boxCollider.bounds.min.y);
+		bottomRight = new Vector2(boxCollider.bounds.max.x, boxCollider.bounds.min.y);
+		topLeft = new Vector2(boxCollider.bounds.min.x, boxCollider.bounds.max.y);
+		topRight = new Vector2(boxCollider.bounds.max.x, boxCollider.bounds.max.y);
+	}
+    // Update is called once per frame
+
+	public void Move(Vector2 velocity)
+	{
+		ComputeBounds();
+		HorizontalMove(ref velocity);
+		transform.Translate(velocity);
+	}
+
+	public void HorizontalMove(ref Vector2 velocity)
+	{
+		float direction = Mathf.Sin(velocity.x);
+		float distance = Mathf.Abs(velocity.x);
+			for (int i = 0; i < verticalRayCount; i++)
+			{
+			Vector2 baseOrigin = direction == 1 ? bottomRight : bottomLeft; 
+				Vector2 origin = bottomRight + new Vector2(0, verticalRaySpacing * i);
+				Debug.DrawLine(origin, origin + new Vector2(velocity.x, 0));
+				RaycastHit2D hit = Physics2D.Raycast(origin, Vector2.right, velocity.x, _layerMask);
+				if (hit)
+				{
+				velocity.x = hit.distance * direction;
+					Debug.Log(hit.point);
+				}
+			}
+	}
+
+}
---
+++ Scripts/Player.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(MovementController))]
+public class Player : MonoBehaviour
+{
+	[Tooltip("Alors, toujours pédé?")]
+	public int _speed = 1;
+	Vector2 _velocity;
+	MovementController mc;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		mc = GetComponent<MovementController>();
+
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+		int horizontal = 0;
+		int vertical = 0;
+		if (Input.GetKey(KeyCode.Q))
+		{
+			horizontal += -1; 
+		}
+		if (Input.GetKey(KeyCode.D))
+		{
+			horizontal += 1;
+		}
+		if (Input.GetKey(KeyCode.S))
+		{
+			vertical += -1;
+		}
+		if (Input.GetKey(KeyCode.Z))
+		{
+			vertical += 1;
+		}
+
+		_velocity = new Vector2(horizontal * _speed, vertical * _speed);
+
+		mc.Move(_velocity * Time.deltaTime);
+	}
+}
